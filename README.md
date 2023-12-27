68436b28b3d22ea071481eda0e0da81a22738ef6
Dev-Saif-Ops <mohammadsafwanathar27@gmail.com>
2023-12-27 21:33:22 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# TradeBook Analyzer
+
+![GitHub](https://img.shields.io/github/license/Dev-Saif-Ops/tradebook-analyzer)
+![GitHub last commit](https://img.shields.io/github/last-commit/Dev-Saif-Ops/tradebook-analyzer)
+![GitHub issues](https://img.shields.io/github/issues-raw/Dev-Saif-Ops/tradebook-analyzer)
+
+An open-source tool for analyzing tradebooks and fetching additional financial data using yfinance.
+
+## Overview
+
+TradeBook Analyzer is a Python-based tool that simplifies the analysis of tradebooks and enhances it by fetching additional financial data for listed stocks. The tool utilizes the powerful `pandas` library for data manipulation and `yfinance` for fetching stock data.
+
+### Features
+
+- **Tradebook Analysis**: Reads and analyzes tradebook data from a CSV file.
+- **Data Enhancement**: Fetches additional stock data based on the 'Symbol' column using yfinance.
+- **Customizable**: Easily adapt the tool for different tradebook formats.
+
+## Getting Started
+
+Follow these steps to set up and run TradeBook Analyzer on your local machine.
+
+### Prerequisites
+
+- Python 3.x
+- Install required packages: `pandas`, `yfinance`, `pytz`
+
+  ```bash
+  pip install pandas yfinance pytz
+
---
+++ tradebook_analyzer.py
@@ -0,0 +1,50 @@
+#Import Necessary things...
+
+import pandas as pd
+import yfinance as yf
+from pytz import timezone
+from datetime import datetime
+
+# Replace 'your_file.csv' with the actual filename (you can put yours...)
+csv_file_path = '/home/hi-tech/Downloads/tradebook-IM8853-EQ.csv'
+
+# Read the CSV file into a Pandas DataFrame
+df = pd.read_csv(csv_file_path)
+
+# List of columns to be removed(You can add what you want to remove)
+columns_to_remove = ['ISIN', 'Segment', 'Series', 'Auction', 'Trade ID', 'Order ID']  # Replace with actual column names
+
+# Drop the specified columns
+df = df.drop(columns=columns_to_remove, errors='ignore')
+
+# Create a new column "Amount" and calculate cumulative sum based on conditions
+df['Amount'] = df.apply(lambda row: row['Quantity'] * row['Price'] if row['Trade Type'] == 'buy' else -row['Quantity'] * row['Price'], axis=1).cumsum()
+
+# Use yfinance to fetch additional data based on 'Symbol' column
+for symbol in df['Symbol'].unique():
+    try:
+        # Determine the exchange suffix based on 'Exchange' column
+        exchange_suffix = '.BO' if df.loc[df['Symbol'] == symbol, 'Exchange'].iloc[0] == 'BSE' else '.NS'
+        
+        # Add the determined suffix to the symbol
+        symbol_bo = f"{symbol}{exchange_suffix}"
+
+        stock_data = yf.download(symbol_bo, start="2023-03-08", end="2023-12-11", ignore_tz=True)
+
+        # Check if the DataFrame is not empty
+        if not stock_data.empty:
+            # Convert the timezone to 'Asia/Kolkata'
+            stock_data.index = stock_data.index.tz_localize('UTC').tz_convert(timezone('Asia/Kolkata'))
+
+            # Do something with stock_data, e.g., print it
+            print(f"Additional data for {symbol}:\n{stock_data}")
+        else:
+            print(f"No data available for {symbol}")
+    except Exception as e:
+        print(f"Failed to fetch data for {symbol}: {e}")
+
+# Replace 'output_file.csv' with the desired name for your CSV file
+output_csv_path = '/home/hi-tech/tradebooks/output_file.csv'  #give your desired location
+
+# Convert the DataFrame to a CSV file
+df.to_csv(output_csv_path, index=False)
